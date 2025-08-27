1c547d9a9ea53b88e466757625042521373484e0
Muhammad Anas Majeed Rafique <muhmajee@car8s3.42barcelona.com>
2025-08-27 18:15:37 +0200

    C04

--- ex03/ft_atoi.c
+++ ex03/ft_atoi.c
@@ -6,7 +6,47 @@
 /*   By: muhmajee <muhmajee@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 00:06:45 by muhmajee          #+#    #+#             */
-/*   Updated: 2025/08/23 00:06:45 by muhmajee         ###   ########.fr       */
+/*   Updated: 2025/08/26 15:47:15 by muhmajee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+
+int	ft_isspace(char c)
+{
+	return (c == ' ' || (c >= 9 && c <= 13));
+}
+
+int	ft_atoi(char *str)
+{
+	int	sign;
+	long	result;
+
+	if (!str)
+		return (0);
+	sign = 1;
+	result = 0;
+	while (ft_isspace(*str))
+		str++;
+	while (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			sign = -sign;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		result = result * 10 + (*str - '0');
+		str++;
+	}
+	return ((int)(sign * result));
+}
+
+/*#include <stdio.h>
+int	main(void)
+{
+	char str1[] = " ---+--+1234ab567";
+	printf("atoi: %i\n", atoi(str1));
+	printf("ft_atoi: %i\n", ft_atoi(str1));
+	return (0);
+}*/
---
+++ ex04/ft_putnbr_base.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: muhmajee <muhmajee@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/26 15:49:31 by muhmajee          #+#    #+#             */
+/*   Updated: 2025/08/27 18:12:31 by muhmajee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	ft_isspace(char c)
+{
+	return (c == ' ' || (c >= 9 && c <= 13));
+}
+
+int	ft_base_is_valid(char *base)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	if (!base || !base[0] || !base[1])
+		return (0);
+	while (base[i])
+	{
+		if (base[i] == '+' || base[i] == '-' || ft_isspace(base[i]))
+			return (0);
+		j = i + 1;
+		while (base[j])
+		{
+			if (base[j] == base[i])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+void	ft_putnbr_base(int nbr, char *base)
+{
+	long	n;
+	int		base_len;
+
+	if (!ft_base_is_valid(base))
+		return ;
+	base_len = ft_strlen(base);
+	n = nbr;
+	if (n < 0)
+	{
+		ft_putchar('-');
+		n = -n;
+	}
+	if (n >= base_len)
+		ft_putnbr_base(n / base_len, base);
+	ft_putchar(base[n % base_len]);
+}
+
+/*#include <stdio.h>
+int	main(void)
+{
+	ft_putnbr_base(42, "01");
+	ft_putchar('\n');
+	ft_putnbr_base(255, "0123456789ABCDEF");
+	ft_putchar('\n');
+	ft_putnbr_base(8, "poniguay");
+	return (0);
+}*/
